245a42f77954e19268f8cd54d5c8766e73928430
Eric-Lombardo <ejlanim8or@gmail.com>
2020-02-17 20:49:40 +0000

    created a new route for /urls/:shortURL and created a template for urls_show.ejs ... it displays the long url of :shortURL

--- express_server.js
+++ express_server.js
@@ -23,6 +23,11 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars)
 })
 
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase };
+  res.render("urls_show", templateVars);
+})
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n")
 })
