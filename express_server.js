b69e3a999233c5dd8a054679c8472eda232a026d
Eric-Lombardo <ejlanim8or@gmail.com>
2020-02-17 22:02:21 +0000

    added a new route /urls/new and created a template for a form as a response. I placed /urls/new on top of /urls/:shortURL to avoid confliction

--- express_server.js
+++ express_server.js
@@ -23,10 +23,15 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars)
 })
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase };
   res.render("urls_show", templateVars);
 })
+
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n")
