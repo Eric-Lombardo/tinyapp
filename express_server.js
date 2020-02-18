0354442720b9ea734bcc6ab2e447045b984e70af
Eric-Lombardo <ejlanim8or@gmail.com>
2020-02-18 23:50:33 +0000

    applied the logout for the POST form button and added some bootstrap styles to make the logout button red

--- express_server.js
+++ express_server.js
@@ -93,6 +93,12 @@ app.post("/login", (req, res) => {
   res.redirect("/urls");
 })
 
+// deletes username cookie from history
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+})
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
